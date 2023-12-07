5234c73ed8d12f80c0086e388f1038e5296c3dda
Shreyank031 <shreyankchandu@gmail.com>
2023-12-07 18:29:22 +0530

    Configured github actions

--- Functions-modules-packages/without-fun.py
+++
@@ -1,8 +0,0 @@
-n1=10
-n2=2
-
-add = n1 + n2
-print(add)
-
-sub = n1-n2
-print(sub)
---
+++ Functions-modules-packages/without_fun.py
@@ -0,0 +1,9 @@
+"""
+Simple script to perform airthmetic operations
+"""
+N1=10
+N2=2
+ADD = N1 + N2
+print(ADD)
+SUB = N1-N2
+print(SUB)
