74b48520df8ce62e58386374d923ee7b3db9b99d
Shreyank031 <shreyankchandu@gmail.com>
2023-12-09 12:18:49 +0530

    Added projects folder

---
+++ projects/README.md
@@ -0,0 +1,5 @@
+# In this specific folder I'll be creating projects based on Python.
+
+## Simple Caesar Cipher Encryption/Decryption:
+
+This Python script demonstrates a basic encryption and decryption technique using the Caesar cipher.
---
+++ projects/caesar_cipher.py
@@ -0,0 +1,20 @@
+# Simple encode-decode project.
+# Incomplete
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text,shift_no):
+  new_word=""
+  for letter in plain_text :
+    position     = alphabet.index(letter)
+    new_position = position + shift_no
+    new_letter   = alphabet[new_position]
+    new_word    += new_letter
+  print(f"The encoded word is {new_word}")
+
+encrypt(plain_text=text, shift_no=shift)
