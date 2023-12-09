a9de29ebae00bfb4be77961b3cb01fabc674269f
Shreyank031 <shreyankchandu@gmail.com>
2023-12-09 16:06:13 +0530

    reorganized the code

--- projects/caesar_cipher.py
+++ projects/caesar_cipher.py
@@ -2,34 +2,39 @@
 # Incomplete
 
 
+# Define the alphabet as a list, including a shifted copy for handling wrapping
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 
             'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
             'w', 'x', 'y', 'z']
 
+# Prompt the user for the encryption/decryption direction ('encode' or 'decode')
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+# Take user input for the message to be encrypted or decrypted and convert it to lowercase
 text = input("Type your message:\n").lower()
+
+# Prompt for the shift number (key) to perform the encryption or decryption
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text,shift_no):
-  new_word=""
-  for letter in plain_text :
-    position     = alphabet.index(letter)
-    new_position = position + shift_no
-    new_letter   = alphabet[new_position]
-    new_word    += new_letter
-  print(f"The encoded word is {new_word}")
-
-def decrypt(encrypted_text,shift_no):
-    decrypted_word = ""
-    for letter in encrypted_text:
-        position         = alphabet.index(letter)
-        new_position     = position - shift_no
-        decrypted_letter = alphabet[new_position]
-        decrypted_word  += decrypted_letter
-    print(f"The decrypted word is {decrypted_word}")
+# Function to perform the Caesar cipher encryption/decryption
+def caesar(start_text, shift_no, caesar_direction):
+    new_word = ""  # Initialize an empty string to store the encrypted/decrypted word
     
-if direction == 'encode':
-    encrypt(plain_text=text,shift_no=shift)
-elif direction == 'decode':
-    decrypt(encrypted_text=text,shift_no=shift)
+    if caesar_direction == "decode":  # Reverse the shift direction for decryption
+        shift_no *= -1
+
+    for letter in start_text:  # Loop through each letter in the input text
+        if letter in alphabet:  # Check if the letter is in the defined alphabet
+            position = alphabet.index(letter)  # Find the index of the letter in the alphabet
+            new_position = position + shift_no  # Shift the position by the specified amount
+            new_letter = alphabet[new_position]  # Get the new letter based on the shifted position
+            new_word += new_letter  # Append the new letter to the encrypted/decrypted word
+        else:
+            new_word += letter  # If the character is not in the alphabet, keep it unchanged
+    
+    print(f"The {caesar_direction} word is {new_word}")  # Print the result of encryption/decryption
+
+# Call the caesar function with the provided inputs
+caesar(start_text=text, shift_no=shift, caesar_direction=direction)
+
